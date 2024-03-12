e976d7ec2f0c786ad64a2084d33c11d71694abc1
latercomer <latercomer@qq.com>
2024-03-12 13:39:19 +0800

    init.bat中增加了promp提示符

--- init.bat
+++ init.bat
@@ -1,5 +1,40 @@
 @echo off
 
+title NextPilot Windows Toolchain
+
+rem Simple "ver" prints empty line before Windows version
+rem Use this construction to print just a version info
+cmd /d /c ver | "%windir%\system32\find.exe" "Windows"
+
+rem Now we form the command prompt
+rem This will start prompt with `User@PC `
+set Prompt0=$E[m$E[32m$E]9;8;"USERNAME"$E\@$E]9;8;"COMPUTERNAME"$E\$S
+
+rem Followed by colored `Path`
+set PROMPT1=%prompt0%$E[92m$P$E[90m
+if NOT "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
+  if "%PROCESSOR_ARCHITEW6432%" == "AMD64" if "%PROCESSOR_ARCHITECTURE%" == "x86" (
+    rem Use another text color if cmd was run from SysWow64
+    set PROMPT1=%prompt0%$E[93m$P$E[90m
+  )
+)
+
+rem Carriage return and `$` or `>`
+rem Spare `$E[90m` was specially added because of GitShowBranch.cmd
+if "%ConEmuIsAdmin%" == "ADMIN" (
+  set PROMPT2=$_$E[90m$$
+) else (
+  set PROMPT2=$_$E[90m$G
+)
+
+rem Finally reset color and add space
+set PROMPT3=$E[m$S$E]9;12$E\
+prompt %prompt1%%prompt2%%prompt3%
+
+
+REM ============= Nextpilot Toolchain Path ==================
+
+
 @REM Setlocal ENABLEDELAYEDEXPANSION
 
 @REM ENV_ROOT
