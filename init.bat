f8c05e3e99db4ab3a6b8708e6bce7bf1606aa169
latercomer <latercomer@qq.com>
2024-03-12 10:17:20 +0800

    完善了启动脚本

---
+++ init.bat
@@ -0,0 +1,31 @@
+@echo off
+
+@REM Setlocal ENABLEDELAYEDEXPANSION
+
+@REM ENV_ROOT
+set ENV_ROOT=%~dp0
+
+set PATH=%SystemRoot%\system32;%PATH%
+set PATH=C:\WINDOWS\system32;%PATH%
+
+@REM python
+set PYTHONPATH=%ENV_ROOT%\toolchain\python\python-3.10.11-embed-amd64
+set PYTHONHOME=%ENV_ROOT%\toolchain\python\python-3.10.11-embed-amd64
+set SCONS=%PYTHONPATH%\Scripts
+set PATH=%PYTHONPATH%;%SCONS%;%PATH%
+
+@REM arm-gcc
+set RTT_EXEC_PATH=%ENV_ROOT%\toolchain\gcc\gcc-arm-none-eabi-10.3-2021.10\bin
+set RTT_CC=gcc
+set PATH=%RTT_EXEC_PATH%;%PATH%
+
+@REM git
+set PATH=%ENV_ROOT%\toolchain\git\MinGit-2.42.0.2-64-bit\cmd;%PATH%
+
+@REM mconf
+set PATH=%ENV_ROOT%\toolchain\mconf\mconf-v4.6.0.0-idf-20190628-win32;%PATH%
+
+@REM env和pkgs
+set PKGS_ROOT=%ENV_ROOT%\rtthread\pkg
+set PKGS_DIR=%ENV_ROOT%\rtthread\pkg
+set PATH=%ENV_ROOT%\rtthread\bin;%PATH%
