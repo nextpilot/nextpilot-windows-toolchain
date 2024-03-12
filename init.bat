e10823dceb2c37b2569ab0ef034bdcb2d28a54d3
latercomer <latercomer@qq.com>
2024-03-12 14:31:20 +0800

    该是改成ENV_ROOT，否则rtt-env脚本没法使用

--- init.bat
+++ init.bat
@@ -37,29 +37,29 @@ REM ============= Nextpilot Toolchain Path ==================
 
 @REM Setlocal ENABLEDELAYEDEXPANSION
 
-@REM BAT_ROOT
-set BAT_ROOT=%~dp0
+@REM ENV_ROOT
+set ENV_ROOT=%~dp0
 
 set PATH=%SystemRoot%\system32;%PATH%
 set PATH=C:\WINDOWS\system32;%PATH%
 
 @REM python
-set PYTHONHOME=%BAT_ROOT%\toolchain\python\python-3.10.11-embed-amd64
+set PYTHONHOME=%ENV_ROOT%\toolchain\python\python-3.10.11-embed-amd64
 set PYTHONPATH=%PYTHONHOME%
 set PATH=%PYTHONHOME%;%PYTHONHOME%\Scripts;%PATH%
 
 @REM arm-gcc
-set RTT_EXEC_PATH=%BAT_ROOT%\toolchain\gcc\gcc-arm-none-eabi-10.3-2021.10\bin
+set RTT_EXEC_PATH=%ENV_ROOT%\toolchain\gcc\gcc-arm-none-eabi-10.3-2021.10\bin
 set RTT_CC=gcc
 set PATH=%RTT_EXEC_PATH%;%PATH%
 
 @REM git
-set PATH=%BAT_ROOT%\toolchain\git\MinGit-2.42.0.2-64-bit\cmd;%PATH%
+set PATH=%ENV_ROOT%\toolchain\git\MinGit-2.42.0.2-64-bit\cmd;%PATH%
 
 @REM mconf
-set PATH=%BAT_ROOT%\toolchain\mconf\kconfig-frontends-3.12.0-windows;%PATH%
+set PATH=%ENV_ROOT%\toolchain\mconf\kconfig-frontends-3.12.0-windows;%PATH%
 
 @REM env和pkgs
-set PKGS_ROOT=%BAT_ROOT%\rtthread\pkg
-set PKGS_DIR=%BAT_ROOT%\rtthread\pkg
-set PATH=%BAT_ROOT%\rtthread\bin;%PATH%
+set PKGS_ROOT=%ENV_ROOT%\rtthread\pkg
+set PKGS_DIR=%ENV_ROOT%\rtthread\pkg
+set PATH=%ENV_ROOT%\rtthread\bin;%PATH%
